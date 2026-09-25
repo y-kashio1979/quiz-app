b7e7c58f8663586c75ab7bb34d3c36c8e6ef7624
y-kashio <y-kashio@exec.co.jp>
2026-09-25 10:14:53 +0900

    スマホ対応

--- src/quiz/components/HistoryComponet.tsx
+++ src/quiz/components/HistoryComponet.tsx
@@ -10,9 +10,11 @@ export const HistoryComponent = ({ histories }: Props) => {
     <>
       {histories && histories.length > 0 && (
         <div className="mt-10">
-          <h2 className="text-xl font-bold text-slate-800 mb-4 text-center">回答履歴</h2>
+          <h2 className="text-xl font-bold text-slate-800 mb-4 text-center">
+            回答履歴
+          </h2>
 
-          <div className="grid grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             {histories.map((history, index) => (
               <HistoryItem key={history.id} no={index + 1} history={history} />
             ))}
